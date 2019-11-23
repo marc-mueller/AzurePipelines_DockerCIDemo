eb47c5bf394d3b91034ce6ae1aea43a7465dc12c
Marc Müller <mmueller@4tecture.ch>
2019-11-23 17:32:18 +0000

    Merged PR 31: Pipeline-Chaining

---
+++ DevFun.Web/DevFun.Web.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevFun.Web.Unit.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
